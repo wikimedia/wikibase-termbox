a0ee672d1ad496d73614cad92cffd5e0590f44d9
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-10-09 14:28:49 +0200

    Initial setup.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    Hello world!
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
