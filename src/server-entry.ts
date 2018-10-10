19d7504d35001df1dafe73883bac9545c699cf19
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-10-10 17:01:04 +0200

    Generate server manifest for vue SSR

---
+++ src/server-entry.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+const app = new Vue( {
+  store,
+  render: ( h ) => h( App ),
+} );
+
+export default app;
