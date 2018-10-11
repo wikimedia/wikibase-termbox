1c173aaad36789f7c1052a415c16bef7a8475c0a
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-10-11 16:08:01 +0200

    Set up an API that renders the App component.

--- src/server-entry.ts
+++ src/server-entry.ts
@@ -4,9 +4,13 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
-const app = new Vue( {
-  store,
-  render: ( h ) => h( App ),
-} );
+const app = new App();
 
-export default app;
+export default ( context: object ) => {
+  // Setting the data here is a bit silly, and should be considered more of a proof of concept.
+  // The better way is likely to fill the store with data from the context object instead.
+  Object.entries( context )
+    .forEach( ( [key, value] ) => app.$data[key] = value );
+
+  return app;
+};
