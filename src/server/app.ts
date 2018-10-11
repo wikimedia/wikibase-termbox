ad8b176ee08588245785b0fd6f7330c1bbe2c8b4
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-10-11 17:12:08 +0200

    Add server test.

---
+++ src/server/app.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import { createBundleRenderer } from 'vue-server-renderer';
+
+const app = express();
+const serverBundle = fs.readFileSync( './dist/vue-ssr-server-bundle.json' );
+const renderer = createBundleRenderer(
+    JSON.parse( serverBundle.toString() ),
+    { runInNewContext: false },
+ );
+
+app.get( '/termbox', ( _, res ) => {
+    const context = {
+        message: 'Hello from server',
+    };
+    renderer.renderToString( context, ( err, html ) => {
+        if ( err ) {
+            console.log( err );
+        }
+        res.send( html );
+    } );
+} );
+
+export default app;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,27 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import express from 'express';
-import { createBundleRenderer } from 'vue-server-renderer';
+import app from './app';
 
-const app = express();
 const port = 3030;
-const serverBundle = fs.readFileSync( './dist/vue-ssr-server-bundle.json' );
-const renderer = createBundleRenderer(
-    JSON.parse( serverBundle.toString() ),
-    { runInNewContext: false },
- );
-
-app.get( '/termbox', ( _, res ) => {
-    const context = {
-        message: 'Hello from server',
-    };
-    renderer.renderToString( context, ( err, html ) => {
-        if ( err ) {
-            console.log( err );
-        }
-        res.send( html );
-    } );
-} );
 
 app.listen( 3030, () => {
     console.log( `server is running at port ${port}` );
