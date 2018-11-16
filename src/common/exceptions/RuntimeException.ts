0a303477d03b0dc7851b80fb58292fda86833779
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-11-16 12:13:45 +0100

    Remove unused files

--- src/common/exceptions/RuntimeException.ts
+++
@@ -1,7 +0,0 @@
-import LogicException from '@/common/exceptions/LogicException';
-
-export default class RuntimeException extends LogicException {
-	constructor( message?: string ) {
-		super( message );
-	}
-}
