d333af2c3097055e842f14ba07d30b4fde73d9c9
Pablo Grass <pablo.grass@wikimedia.de>
2018-11-20 10:56:57 +0100

    lint: apply linting to JS files, too
    Apply rules defined for JS files.

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsConfigFile: './tsconfig.all.json',
-    },
-  },
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
-    'vue',
-    'ts',
-    'tsx'
-  ],
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  setupTestFrameworkScriptFile: '<rootDir>/tests/config/setup.ts',
-  snapshotSerializers: [
-    'jest-serializer-vue'
-  ],
-  testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  ],
-  testURL: 'http://localhost/'
+	globals: {
+		'ts-jest': {
+			tsConfigFile: './tsconfig.all.json',
+		},
+	},
+	moduleFileExtensions: [
+		'js',
+		'jsx',
+		'json',
+		'vue',
+		'ts',
+		'tsx',
+	],
+	transform: {
+		'^.+\\.vue$': 'vue-jest',
+		'.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+		'^.+\\.tsx?$': 'ts-jest',
+	},
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/src/$1',
+	},
+	setupTestFrameworkScriptFile: '<rootDir>/tests/config/setup.ts',
+	snapshotSerializers: [
+		'jest-serializer-vue',
+	],
+	testMatch: [
+		'**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+	],
+	testURL: 'http://localhost/',
 };
--- vue.config.js
+++ vue.config.js
@@ -1,53 +1,53 @@
-const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
-const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const VueSSRServerPlugin = require( 'vue-server-renderer/server-plugin' );
+const VueSSRClientPlugin = require( 'vue-server-renderer/client-plugin' );
 
-const TARGET_NODE = process.env.WEBPACK_TARGET === 'node'
-const DEV_MODE = process.env.WEBPACK_TARGET === 'dev'
-const filePrefix = 'wikibase.termbox.'
+const TARGET_NODE = process.env.WEBPACK_TARGET === 'node';
+const DEV_MODE = process.env.WEBPACK_TARGET === 'dev';
+const filePrefix = 'wikibase.termbox.';
 
 const target = TARGET_NODE
-  ? 'server'
-  : 'client'
+	? 'server'
+	: 'client';
 
 module.exports = {
-  outputDir: TARGET_NODE ? 'serverDist' : 'dist',
-  configureWebpack: () => ({
-	entry: DEV_MODE ? [ './src/mockup-entry.ts', `./src/${target}-entry.ts` ] : `./src/${target}-entry.ts`,
-    target: TARGET_NODE ? 'node' : 'web',
-    node: TARGET_NODE ? undefined : false,
-    plugins: [
-      TARGET_NODE
-        ? new VueSSRServerPlugin()
-        : new VueSSRClientPlugin()
-    ],
-    output: {
-      libraryTarget: TARGET_NODE
-        ? 'commonjs2'
-        : undefined,
-      filename: `${filePrefix}[name].js`
-    },
-    optimization: {
-      splitChunks: undefined
-    }
-  }),
-  chainWebpack: config => {
-    config.optimization.delete('splitChunks')
+	outputDir: TARGET_NODE ? 'serverDist' : 'dist',
+	configureWebpack: () => ( {
+		entry: DEV_MODE ? [ './src/mockup-entry.ts', `./src/${target}-entry.ts` ] : `./src/${target}-entry.ts`,
+		target: TARGET_NODE ? 'node' : 'web',
+		node: TARGET_NODE ? undefined : false,
+		plugins: [
+			TARGET_NODE
+				? new VueSSRServerPlugin()
+				: new VueSSRClientPlugin(),
+		],
+		output: {
+			libraryTarget: TARGET_NODE
+				? 'commonjs2'
+				: undefined,
+			filename: `${filePrefix}[name].js`,
+		},
+		optimization: {
+			splitChunks: undefined,
+		},
+	} ),
+	chainWebpack: config => {
+		config.optimization.delete( 'splitChunks' );
 
-    if ( process.env.NODE_ENV === 'production' ) {
-      config.plugin('extract-css')
-        .tap(([options, ...args]) => [
-            Object.assign({}, options, { filename: `${filePrefix}[name].css` }),
-            ...args
-        ])
-    }
+		if ( process.env.NODE_ENV === 'production' ) {
+			config.plugin( 'extract-css' )
+				.tap( ( [ options, ...args ] ) => [
+					Object.assign( {}, options, { filename: `${filePrefix}[name].css` } ),
+					...args,
+				] );
+		}
 
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options =>
-        Object.assign(options, {
-          optimizeSSR: false
-        })
-      )
-  }
-}
+		config.module
+			.rule( 'vue' )
+			.use( 'vue-loader' )
+			.tap( options =>
+				Object.assign( options, {
+					optimizeSSR: false,
+				} ),
+			);
+	},
+};
