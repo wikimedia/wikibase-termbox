8e1513562dfa3a90fe53a3006de4f778d3f27a16
Pablo Grass <pablo.grass@wikimedia.de>
2018-11-21 20:25:19 +0100

    align mock with changes to mw.config access

--- tests/unit/client/init.spec.ts
+++ tests/unit/client/init.spec.ts
@@ -3,10 +3,19 @@ import init from '@/client/init';
 import TermboxRequest from '@/common/TermboxRequest';
 import MwWindow from '../../../src/client/MwWindow';
 
-function mockMwEnv( language: string, entity: any ) {
+function mockMwEnv( language: string, entityId: any ) {
 	( window as MwWindow ).mw = {
-		config: { get() { return language; } },
-		hook: () => new ImmediatelyInvokingEntityLoadedHookHandler( entity ),
+		config: { get( key ) {
+			switch ( key ) {
+				case 'wgUserLanguage':
+					return language;
+				case 'wbEntityId':
+					return entityId;
+				default:
+					return null;
+			}
+		} },
+		hook: () => new ImmediatelyInvokingEntityLoadedHookHandler( {} ),
 	};
 }
 
@@ -20,22 +29,12 @@ describe( 'client/init', () => {
 		expect( termboxRequestPromise ).resolves.toBeInstanceOf( TermboxRequest );
 	} );
 
-	it( 'loads a TermboxRequest from the mw environment', async () => {
-		const entity = {
-			id: 'Q666',
-			labels: { en: { language: 'en', value: 'duck' } },
-			descriptions: { en: { language: 'en', value: 'aquatic bird' } },
-			aliases: { en: [{ language: 'en', value: 'floaty bird' }] },
-		};
-
-		mockMwEnv( 'en', entity );
+	it( 'generates a TermboxRequest from the mw environment', async () => {
+		mockMwEnv( 'en', 'Q666' );
 
 		return init().then( ( request ) => {
 			expect( request.language ).toBe( 'en' );
-			expect( request.entity.id ).toBe( entity.id );
-			expect( request.entity.labels ).toBe( entity.labels );
-			expect( request.entity.descriptions ).toBe( entity.descriptions );
-			expect( request.entity.aliases ).toBe( entity.aliases );
+			expect( request.entityId ).toBe( 'Q666' );
 		} );
 	} );
 
