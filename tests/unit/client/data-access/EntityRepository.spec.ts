eaa5f64515b0516863afd0eaa4d965a5a6ffa350
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-11-23 16:49:28 +0100

    Test client/EntityRepository

---
+++ tests/unit/client/data-access/EntityRepository.spec.ts
@@ -0,0 +1,34 @@
+import EntityRepository from '@/client/data-access/EntityRepository';
+import MwWindow from '@/client/MwWindow';
+import MwConfig from '@/mock-data/MwConfig';
+import FingerprintableEntity from '@/datamodel/FingerprintableEntity';
+import ImmediatelyInvokingEntityLoadedHookHandler from '@/mock-data/ImmediatelyInvokingEntityLoadedHookHandler';
+
+describe( 'EntityRepository', () => {
+
+	it( 'getFingerPrintableEntity returns entity from entityLoaded hook', () => {
+		const entityRepository = new EntityRepository();
+		const entityReturnedFromHook = {
+			id: 'Q123',
+			labels: { en: { language: 'en', value: 'potato' } },
+			descriptions: { en: { language: 'en', value: 'root vegetable' } },
+			aliases: { en: [ { language: 'en', value: 'patata' } ] },
+		};
+		( window as MwWindow ).mw = {
+			hook() {
+				return new ImmediatelyInvokingEntityLoadedHookHandler( entityReturnedFromHook );
+			},
+			config: new MwConfig(),
+		};
+
+		return entityRepository.getFingerprintableEntity( 'Q123' ).then( ( entity: FingerprintableEntity ) => {
+			expect( entity ).toBeInstanceOf( FingerprintableEntity );
+
+			expect( entity.id ).toBe( entityReturnedFromHook.id );
+			expect( entity.labels ).toBe( entityReturnedFromHook.labels );
+			expect( entity.descriptions ).toBe( entityReturnedFromHook.descriptions );
+			expect( entity.aliases ).toBe( entityReturnedFromHook.aliases );
+		} );
+	} );
+
+} );
