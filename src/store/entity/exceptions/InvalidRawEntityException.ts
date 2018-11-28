914776b1415b4ec87a7c8c7057f06f8316645afc
Pablo Grass <pablo.grass@wikimedia.de>
2018-11-28 11:20:34 +0100

    InvalidRawEntityException: rm unused file
    Remove unused file containing an exception class definition not matching
    its file name.

--- src/store/entity/exceptions/InvalidRawEntityException.ts
+++
@@ -1,7 +0,0 @@
-import ParameterException from '@/common/exceptions/ParameterException';
-
-export default class InvalidEntityException extends ParameterException {
-	constructor( message?: string ) {
-		super( message );
-	}
-}
