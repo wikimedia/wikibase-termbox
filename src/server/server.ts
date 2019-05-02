d408496a745b4998d5a8dfc8bdb2d75c64df5d67
Pablo Grass <pablo.grass@wikimedia.de>
2019-05-02 15:28:23 +0200

    server: consolidate config validation
    Consolidate the way use of default values is reported when starting up.
    This also allows for config values (like MEDIAWIKI_REQUEST_TIMEOUT)
    to be implicitly false (e.g. 0) which caused the use of default values
    before.
    
    Bug: T218080
    Change-Id: Ia3a9e1467bb42f053af122c9058f5a83c1ede5df

--- src/server/server.ts
+++ src/server/server.ts
@@ -10,23 +10,28 @@ import ServiceRunnerOptions from './ServiceRunnerOptions';
 
 /* eslint-disable no-console */
 
-function verifyAndReportSetting( name: string, value: any ) {
+function assertAndGetSetting( config: { [ index: string ]: any }, name: string, fallback?: any ): any {
+	let value = config[ name ];
+
 	if ( typeof value === 'undefined' ) {
-		console.warn( `${name} env must be configured to a meaningful value. Exiting.` );
-		process.exit( 1 );
+		if ( typeof fallback === 'undefined' ) {
+			console.warn( `${name} env must be configured to a meaningful value. Exiting.` );
+			process.exit( 1 );
+		}
+
+		value = fallback;
 	}
 
-	console.info( `Set ${name} env to ${value}` );
+	console.info( `Set ${name} to ${value}` );
+
+	return value;
 }
 
 export default ( options: ServiceRunnerOptions ) => {
-	const wikibaseRepo = options.config.WIKIBASE_REPO;
-	const ssrPort = options.config.SSR_PORT;
-	const serverRequestTimeout = options.config.MEDIAWIKI_REQUEST_TIMEOUT || DEFAULT_REQUEST_TIMEOUT;
-
-	verifyAndReportSetting( 'WIKIBASE_REPO', wikibaseRepo );
-	verifyAndReportSetting( 'SSR_PORT', ssrPort );
-	verifyAndReportSetting( 'MEDIAWIKI_REQUEST_TIMEOUT', serverRequestTimeout );
+	const config = options.config;
+	const wikibaseRepo = assertAndGetSetting( config, 'WIKIBASE_REPO' );
+	const ssrPort = assertAndGetSetting( config, 'SSR_PORT' );
+	const serverRequestTimeout = assertAndGetSetting( config, 'MEDIAWIKI_REQUEST_TIMEOUT', DEFAULT_REQUEST_TIMEOUT );
 
 	const services = new BundleRendererServices(
 		axios.create( {
