7b725758c3e7fe1d6482d451f50ed3fac7ff022f
Pablo Grass <pablo.grass@wikimedia.de>
2019-05-14 16:22:55 +0200

    Modal: add storybook
    Bug: T222697
    Change-Id: I00f29e6ae68152d9a31d0fff78a35bb6b5ec585c

---
+++ stories/Modal.js
@@ -0,0 +1,10 @@
+import { storiesOf } from '@storybook/vue';
+import Modal from '@/components/Modal.vue';
+
+storiesOf( 'Modal', module )
+	.add( 'default', () => ( {
+		components: { Modal },
+		template: `<Modal>
+			<p>Nullam pharetra lacus vel urna elementum posuere. Nunc volutpat mattis augue. Sed eu enim vitae dui malesuada sodales et eget mi. Maecenas molestie fringilla eros, quis eleifend tortor dignissim eu. Integer elementum sodales neque, vitae venenatis justo. In molestie fermentum tortor, quis ornare elit dictum eget. Aliquam accumsan placerat ornare. Suspendisse mollis nibh eu lectus laoreet venenatis. Nulla dignissim ultrices magna, eu viverra ipsum semper nec. Fusce viverra aliquam vehicula. Curabitur maximus posuere maximus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus nec laoreet nulla. Aliquam convallis quam mauris, non posuere urna mattis eget. Ut rhoncus venenatis dui, sit amet sollicitudin quam lobortis sit amet.</p>
+		</Modal>`,
+	} ) );
