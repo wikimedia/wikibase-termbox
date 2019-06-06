805ae8d0de1af390618c141bc47d78d8a827402b
bitPogo <matthias.geisler@wikimedia.de>
2019-06-06 17:02:46 +0200

    Run updates via systemd
    This patch get rid of the build dependencies as well as extra crontabed
    update.
    It's based on Ic57303895da4ba9491e5993226ec73e5250535b9
    
    Change-Id: I9a66e9a5b9a1ed4ed434684bbca7d8919c0a9f79

---
+++ infrastructure/files/restarter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+if $( sudo systemctl is-active --quiet termbox ); then
+	LATEST_COMMIT=$(curl -s --header 'Accept: application/vnd.github.v3.sha' 'https://api.github.com/repos/wikimedia/wikibase-termbox/commits/master')
+
+	if [[\
+			"$(docker images -q docker-registry.wikimedia.org/wikimedia/wikibase-termbox:$LATEST_COMMIT 2> /dev/null)" == ""\
+		&&\
+			$( curl --output /dev/null --silent --head --fail "https://docker-registry.wikimedia.org/v2/wikimedia/wikibase-termbox/manifests/$LATEST_COMMIT" )\
+	]]; then
+		echo "Restart Termbox SSR"
+		sudo systemctl restart termbox
+	fi
+else
+	echo "Start Termbox"
+	sudo systemctl start termbox
+fi
