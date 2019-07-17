e10976f23545901f8deeef249224120c1317ba14
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2019-07-17 21:28:52 +0100

    Introduce IndeterminateProgressBar
    Introduce component and storybook entries for an indeterminate
    progress bar.
    
    Bug: T222890
    Change-Id: Ib2d60266832357c899c45eaa0709f35eb0606ce5

---
+++ stories/IndeterminateProgressBar.js
@@ -0,0 +1,11 @@
+import { storiesOf } from '@storybook/vue';
+import IndeterminateProgressBar from '@/components/IndeterminateProgressBar.vue';
+
+storiesOf( 'IndeterminateProgressBar', module )
+	.add( 'default', () => ( {
+		components: { IndeterminateProgressBar },
+		template:
+			`<div>
+				<IndeterminateProgressBar />
+			</div>`,
+	} ) );
