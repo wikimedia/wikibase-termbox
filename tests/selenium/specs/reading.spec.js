5d2c2643165be452cbcc7d0e5096ee7e5273f7f1
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2019-08-08 17:41:35 +0200

    browser tests: split Termbox.spec.js
    This also deletes some unimportant tests.
    
    Bug: T229910
    Change-Id: If159d496066db0e20502f1a883ae21e05e5a81c8

---
+++ tests/selenium/specs/reading.spec.js
@@ -0,0 +1,160 @@
+const assert = require( 'assert' );
+const TermboxPage = require( '../pageobjects/Termbox.page' );
+const TermboxLanguages = require( '../TermboxLanguages' );
+const createTermsInLanguages = require( '../createTermsInLanguages' );
+const WikibaseApi = require( 'wdio-wikibase/wikibase.api' );
+
+function assertMonolingualFingerprintHasTermsInLanguage( elements, expectedTerms, language ) {
+	assert.strictEqual(
+		elements.label.getText(),
+		expectedTerms.labels[ language ].value
+	);
+	assert.strictEqual(
+		elements.description.getText(),
+		expectedTerms.descriptions[ language ].value
+	);
+	expectedTerms.aliases[ language ].forEach( ( { value: alias }, j ) => {
+		assert.strictEqual(
+			elements.aliases[ j ].getText(),
+			alias
+		);
+	} );
+}
+
+describe( 'Termbox: reading', () => {
+	const useLangParam = 'de';
+	let terms, id, termboxLanguages;
+
+	before( () => {
+		termboxLanguages = TermboxLanguages.initWithUseLang( useLangParam );
+		terms = createTermsInLanguages(
+			[ useLangParam ]
+				.concat( termboxLanguages.getPreferredLanguages() )
+				.concat( termboxLanguages.getNonPreferredLanguages().slice( 0, 2 ) )
+		);
+		id = browser.call( () => WikibaseApi.createItem( '', terms ) );
+	} );
+
+	beforeEach( () => {
+		browser.deleteCookie();
+		TermboxPage.openItemPage( id, useLangParam );
+	} );
+
+	it( 'is in reading mode when opening the item page', () => {
+		assert.ok( TermboxPage.isInReadMode );
+	} );
+
+	describe( 'primary language terms', () => {
+		it( 'contains the expected language with respective terms', () => {
+			TermboxPage.openItemPage( id, useLangParam );
+
+			const primaryFingerprint = TermboxPage.getMonolingualFingerprintsInSection(
+				TermboxPage.primaryMonolingualFingerprint
+			)[ 0 ];
+
+			assert.strictEqual(
+				primaryFingerprint.language.getText(),
+				termboxLanguages.getContentLanguages()[ useLangParam ]
+			);
+			assertMonolingualFingerprintHasTermsInLanguage(
+				primaryFingerprint,
+				terms,
+				useLangParam
+			);
+		} );
+	} );
+
+	describe( '"in more languages" section', () => {
+		beforeEach( () => {
+			TermboxPage.openItemPage( id, useLangParam );
+		} );
+
+		it( 'has a collapse/expand button', () => {
+			assert.ok( TermboxPage.inMoreLanguagesButton.isVisible() );
+		} );
+
+		it( 'is expanded by default', () => {
+			assert.ok( TermboxPage.inMoreLanguages.isVisible() );
+		} );
+
+		it( 'is collapsible, also hiding the "all entered languages" section', () => {
+			TermboxPage.inMoreLanguagesButton.click();
+
+			assert.strictEqual( TermboxPage.inMoreLanguages.isVisible(), false );
+			assert.strictEqual( TermboxPage.allEnteredLanguagesButton.isVisible(), false );
+		} );
+
+		it( 'expands again when clicking the button twice', () => {
+			TermboxPage.inMoreLanguagesButton.click();
+			TermboxPage.inMoreLanguagesButton.click();
+
+			assert.ok( TermboxPage.inMoreLanguages.isVisible() );
+		} );
+
+		it( 'contains the expected languages with respective terms', () => {
+			const expectedLanguages = termboxLanguages.getPreferredLanguages().slice( 1 );
+			const monolingualFingerprints = TermboxPage.getMonolingualFingerprintsInSection(
+				TermboxPage.inMoreLanguages
+			);
+
+			expectedLanguages.forEach( ( language, i ) => {
+				assert.strictEqual(
+					monolingualFingerprints[ i ].language.getText(),
+					termboxLanguages.getContentLanguages()[ language ]
+				);
+				assertMonolingualFingerprintHasTermsInLanguage(
+					monolingualFingerprints[ i ],
+					terms,
+					language
+				);
+			} );
+		} );
+	} );
+
+	describe( '"all entered languages" section', () => {
+		beforeEach( () => {
+			TermboxPage.openItemPage( id, useLangParam );
+		} );
+
+		it( 'is collapsed by default', () => {
+			assert.strictEqual( TermboxPage.allEnteredLanguages.isVisible(), false );
+		} );
+
+		it( 'has a collapse/expand button', () => {
+			assert.ok( TermboxPage.allEnteredLanguagesButton.isVisible() );
+		} );
+
+		it( 'is expandable', () => {
+			TermboxPage.allEnteredLanguagesButton.click();
+			assert.ok( TermboxPage.allEnteredLanguages.isVisible() );
+		} );
+
+		it( 'collapses again when clicking the button twice', () => {
+			TermboxPage.allEnteredLanguagesButton.click();
+			TermboxPage.allEnteredLanguagesButton.click();
+
+			assert.strictEqual( TermboxPage.allEnteredLanguages.isVisible(), false );
+		} );
+
+		it( 'contains the expected languages with respective terms', () => {
+			TermboxPage.allEnteredLanguagesButton.click();
+
+			const expectedLanguages = termboxLanguages.getNonPreferredLanguages().slice( 0, 2 );
+			const monolingualFingerprints = TermboxPage.getMonolingualFingerprintsInSection(
+				TermboxPage.allEnteredLanguages
+			);
+
+			expectedLanguages.forEach( ( language, i ) => {
+				assert.strictEqual(
+					monolingualFingerprints[ i ].language.getText(),
+					termboxLanguages.getContentLanguages()[ language ]
+				);
+				assertMonolingualFingerprintHasTermsInLanguage(
+					monolingualFingerprints[ i ],
+					terms,
+					language
+				);
+			} );
+		} );
+	} );
+} );
