b2957b730c986f933758a5c77e4be7cadeffe0b3
bitPogo <matthias.geisler@wikimedia.de>
2019-12-20 15:21:03 +0100

    Use knobs
    This patch introduces knobs for termbox.
    For knobs see: https://www.npmjs.com/package/@storybook/addon-knobs
    
    Change-Id: I747b826616db3dd1450772ad6204f167fc4ea884

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,9 @@ import {
 } from '@storybook/vue';
 import '@/client/directives';
 import { withInfo } from 'storybook-addon-vue-info';
+import { withKnobs } from '@storybook/addon-knobs';
 
+addDecorator( withKnobs );
 addDecorator( withInfo );
 
 const req = require.context( '../stories', true, /\.js$/ );
--- stories/EventEmittingButton.js
+++ stories/EventEmittingButton.js
@@ -1,12 +1,25 @@
 import { storiesOf } from '@storybook/vue';
+import {
+	text,
+	boolean,
+} from '@storybook/addon-knobs';
 import EventEmittingButton from '@/components/EventEmittingButton.vue';
 
 storiesOf( 'EventEmittingButton', module )
 	.add(
 		'edit',
 		() => ( {
+			props: {
+				message: {
+					default: text( 'button label', 'edit' ),
+				},
+				preventDefault: {
+					default: boolean( 'as external link', false ),
+				},
+
+			},
 			components: { EventEmittingButton },
-			template: '<EventEmittingButton type="edit" message="edit" />',
+			template: '<EventEmittingButton type="edit" :message="message" :preventDefault="preventDefault"/>',
 		} ),
 		{
 			info: {
@@ -15,31 +28,64 @@ storiesOf( 'EventEmittingButton', module )
 		},
 	)
 	.add( 'cancel', () => ( {
+		props: {
+			message: {
+				default: text( 'button label', 'cancel' ),
+			},
+			preventDefault: {
+				default: boolean( 'as external link', false ),
+			},
+		},
 		components: { EventEmittingButton },
-		template: '<EventEmittingButton type="cancel" message="cancel" />',
+		template: '<EventEmittingButton type="cancel" :message="message"  :preventDefault="preventDefault"/>',
 	} ), { info: true } )
 	.add( 'publish', () => ( {
+		props: {
+			message: {
+				default: text( 'button label', 'publish' ),
+			},
+			preventDefault: {
+				default: boolean( 'as external link', false ),
+			},
+		},
 		components: { EventEmittingButton },
-		template: '<EventEmittingButton type="publish" message="publish" />',
+		template: '<EventEmittingButton type="publish" :message="message" :preventDefault="preventDefault"/>',
 	} ), { info: true } )
 	.add( 'normal', () => ( {
+		props: {
+			message: {
+				default: text( 'button label', 'normal' ),
+			},
+			preventDefault: {
+				default: boolean( 'as external link', false ),
+			},
+		},
 		components: { EventEmittingButton },
-		template: '<EventEmittingButton type="normal" message="normal" />',
+		template: '<EventEmittingButton type="normal" :message="message" :preventDefault="preventDefault"/>',
 	} ), { info: true } )
 	.add( 'primaryProgressive', () => ( {
+		props: {
+			message: {
+				default: text( 'button label', 'primaryProgressive' ),
+			},
+			preventDefault: {
+				default: boolean( 'as external link', false ),
+			},
+		},
 		components: { EventEmittingButton },
-		template: '<EventEmittingButton type="primaryProgressive" message="primaryProgressive" />',
+		template: `<EventEmittingButton type="primaryProgressive"
+						:message="message" :preventDefault="preventDefault"/>`,
 	} ), { info: true } )
 	.add( 'framelessProgressive', () => ( {
+		props: {
+			message: {
+				default: text( 'button label', 'frameless Progressive' ),
+			},
+			preventDefault: {
+				default: boolean( 'as external link', false ),
+			},
+		},
 		components: { EventEmittingButton },
-		template: '<EventEmittingButton type="framelessProgressive" message="framelessProgressive" />',
-	} ), { info: true } )
-	.add( 'framelessProgressive as link', () => ( {
-		components: { EventEmittingButton },
-		template: `<EventEmittingButton
-			type="framelessProgressive"
-			message="go (opens in new tab)"
-			href="https://www.wikidata.org/"
-			:preventDefault="false"
-			target="_blank" />`,
+		template: `<EventEmittingButton type="framelessProgressive"
+						:message="message" :preventDefault="preventDefault"/>`,
 	} ), { info: true } );
