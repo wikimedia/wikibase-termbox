67ebbc857232e3174c79f4f6f00677420237afef
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2021-11-22 18:50:00 +0100

    Convert Messages mixin to options API
    Using a this parameter [1] to declare the types of the mapped getter and
    state in the message() method. All uses of the mixin are in templates,
    as far as I can tell (rather than in script blocks), so we don’t need to
    worry about TypeScript in those.
    
    [1]: https://www.typescriptlang.org/docs/handbook/2/functions.html#declaring-this-in-a-function
    
    Bug: T296202
    Change-Id: I4dfea0863612473e004379fc1e38e670dc91946a

--- src/components/mixins/Messages.ts
+++ src/components/mixins/Messages.ts
@@ -1,29 +1,37 @@
 import Vue from 'vue';
-import Component from 'vue-class-component';
 import { MessageKey } from '@/common/MessageKey';
 import { NS_MESSAGES, NS_USER } from '@/store/namespaces';
-import { namespace } from 'vuex-class';
+import {
+	mapGetters,
+	mapState,
+} from 'vuex';
 
-@Component
-export default class Messages extends Vue {
-	public readonly MESSAGE_KEYS = MessageKey;
-
-	@namespace( NS_MESSAGES ).Getter( 'getMessageInLanguage' )
-	public getMessageInLanguage!: ( inLanguage: string, messageKey: MessageKey ) => string | null;
-
-	@namespace( NS_USER ).State( 'primaryLanguage' )
-	public primaryLanguage!: string;
-
-	public message( messageKey: MessageKey ): string {
-		const messageContent = this.getMessageInLanguage(
-			this.primaryLanguage,
-			messageKey,
-		);
-
-		if ( messageContent === null ) {
-			return messageKey;
-		} else {
-			return messageContent;
-		}
-	}
+interface Messages {
+	getMessageInLanguage: ( inLanguage: string, messageKey: MessageKey ) => string | null;
+	primaryLanguage: string;
 }
+
+export default Vue.extend( {
+	name: 'Messages',
+	computed: {
+		MESSAGE_KEYS() {
+			return MessageKey;
+		},
+		...mapGetters( NS_MESSAGES, [ 'getMessageInLanguage' ] ),
+		...mapState( NS_USER, [ 'primaryLanguage' ] ),
+	},
+	methods: {
+		message( this: Messages, messageKey: MessageKey ): string {
+			const messageContent = this.getMessageInLanguage(
+				this.primaryLanguage,
+				messageKey,
+			);
+
+			if ( messageContent === null ) {
+				return messageKey;
+			} else {
+				return messageContent;
+			}
+		},
+	},
+} );
