61f98dc9c0028308adfa4e6efa79f1eee9eaf9f5
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2021-11-25 12:07:57 +0100

    Convert getChildComponents test to options API
    Bug: T296202
    Change-Id: I8e6346717d6c9f3737acaf699891b57cc8f2a7b0

--- tests/unit/common/getChildComponents.spec.ts
+++ tests/unit/common/getChildComponents.spec.ts
@@ -1,57 +1,28 @@
 import Vue from 'vue';
 import getChildComponents from '@/common/getChildComponents';
-import Component from 'vue-class-component';
 
 /* tslint:disable:max-classes-per-file */
 
 describe( 'getChildComponents', () => {
 
 	it( 'adds the root component', () => {
-		@Component( {
-		} )
-		class Root extends Vue {
-		}
+		const Root = Vue.extend( { name: 'Root' } );
 
 		expect( getChildComponents( new Root() ) ).toEqual( [ Root ] );
 	} );
 
 	it( 'adds children of the root component', () => {
-		@Component( {
-		} )
-		class Child1 extends Vue {
-		}
-
-		@Component( {
-			components: { Child1 },
-		} )
-		class Root extends Vue {
-		}
+		const Child1 = Vue.extend( { name: 'Child1' } );
+		const Root = Vue.extend( { name: 'Root', components: { Child1 } } );
 
 		expect( getChildComponents( new Root() ) ).toEqual( [ Child1, Root ] );
 	} );
 
 	it( 'recursively gets child components', () => {
-		@Component( {
-		} )
-		class NestedChild extends Vue {
-		}
-
-		@Component( {
-			components: { NestedChild },
-		} )
-		class Child1 extends Vue {
-		}
-
-		@Component( {
-		} )
-		class Child2 extends Vue {
-		}
-
-		@Component( {
-			components: { Child1, Child2 },
-		} )
-		class Root extends Vue {
-		}
+		const NestedChild = Vue.extend( { name: 'NestedChild' } );
+		const Child1 = Vue.extend( { name: 'Child1', components: { NestedChild } } );
+		const Child2 = Vue.extend( { name: 'Child2', components: { NestedChild } } );
+		const Root = Vue.extend( { name: 'Root', components: { Child1, Child2 } } );
 
 		const allChildren = getChildComponents( new Root() );
 		expect( allChildren ).toHaveLength( 4 );
