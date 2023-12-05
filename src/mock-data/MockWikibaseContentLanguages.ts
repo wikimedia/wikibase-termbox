b4e53164bf1bfa1d9513b1493fba8b3223366ba9
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2023-12-05 12:42:54 +0100

    Fix missing ContentLanguages.getAll() in dev mode
    This was missing in Ie77a5c130ebc96af189fb75128a600aea78373af.
    
    Change-Id: I9f7efe88a4a7d49e22dd5def3932bf22cb641d3f

--- src/mock-data/MockWikibaseContentLanguages.ts
+++ src/mock-data/MockWikibaseContentLanguages.ts
@@ -2,6 +2,10 @@ import { StringTMap } from '@/datamodel/LanguageTranslations';
 import languageMap from '@/mock-data/data/en_lang_data.json';
 
 export default class MockWikibaseContentLanguages {
+	public getAll(): string[] {
+		return Object.keys( languageMap );
+	}
+
 	public getLanguageNameMap(): StringTMap<string> {
 		return languageMap;
 	}
